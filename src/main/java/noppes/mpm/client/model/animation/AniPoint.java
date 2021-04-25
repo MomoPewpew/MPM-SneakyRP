827aa1c4a2bcafeabbca187804c2a1a3dc014dfd
marijntepas <marijntepas@gmail.com>
2021-04-25 12:23:17 +0200

    Point adjusts to head pitch

--- src/main/java/noppes/mpm/client/model/animation/AniPoint.java
+++ src/main/java/noppes/mpm/client/model/animation/AniPoint.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.Entity;
 
 public class AniPoint {
      public static void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entity, ModelBiped base) {
-          base.bipedRightArm.rotateAngleX = -1.570796F;
+          base.bipedRightArm.rotateAngleX = (headPitch / 57.295776F) - 1.57F;
           base.bipedRightArm.rotateAngleY = netHeadYaw / 57.295776F;
           base.bipedRightArm.rotateAngleZ = 0.0F;
      }

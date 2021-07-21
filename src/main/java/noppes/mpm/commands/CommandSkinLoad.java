7cf923187df469d3a70d92ff33543a74443fe2ec
marijntepas <marijntepas@gmail.com>
2021-07-21 19:07:52 +0200

    Cleanup for CommandSkinLoad

--- src/main/java/noppes/mpm/commands/CommandSkinLoad.java
+++ src/main/java/noppes/mpm/commands/CommandSkinLoad.java
@@ -46,6 +46,8 @@ public class CommandSkinLoad extends CommandBase {
 
              file = new File(dir, filename);
 
+             NBTTagCompound compound = new NBTTagCompound();
+
              if (!file.exists()) {
             	 dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "skins");
 
@@ -55,10 +57,12 @@ public class CommandSkinLoad extends CommandBase {
 
                  file = new File(dir, filename);
 
-                 NBTTagCompound compound = CompressedStreamTools.readCompressed(new FileInputStream(file));
+                 if (file.exists()) {
+                	 NBTTagCompound temp = CompressedStreamTools.readCompressed(new FileInputStream(file));
 
-                 if (!compound.getString("EntityClass").equals("") && MorePlayerModels.playersEntityDenied.contains(((EntityPlayer) icommandsender).getUniqueID()))
-                	 return;
+                     if (!temp.getString("EntityClass").equals("") && MorePlayerModels.playersEntityDenied.contains(((EntityPlayer) icommandsender).getUniqueID()))
+                    	 return;
+                 }
              }
 
              if (!file.exists() && MorePlayerModels.playersEntityDenied.contains(((EntityPlayer) icommandsender).getUniqueID())) {
@@ -74,6 +78,8 @@ public class CommandSkinLoad extends CommandBase {
              if (!file.exists()) {
             	 icommandsender.addChatMessage(new TextComponentTranslation("The skin " + args[0] + " was not found on the server."));
             	 return;
+             } else {
+            	 compound = CompressedStreamTools.readCompressed(new FileInputStream(file));
              }
 
              Server.sendAssociatedData((Entity) icommandsender, EnumPackets.SEND_PLAYER_DATA, ((Entity) icommandsender).getUniqueID(), compound);

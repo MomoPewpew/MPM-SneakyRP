7977cad2945f42e9d41d821fd4b2764539c23e96
marijntepas <marijntepas@gmail.com>
2021-07-21 18:00:28 +0200

    Skindel will now archive a skin rather than delete it

--- src/main/java/noppes/mpm/commands/CommandSkinDel.java
+++ src/main/java/noppes/mpm/commands/CommandSkinDel.java
@@ -36,7 +36,17 @@ public class CommandSkinDel extends CommandBase {
             	 return;
              }
 
-        	 file.delete();
+             File dirnew = null;
+             dirnew = new File(dirnew, ".." + File.separator + "moreplayermodels" + File.separator + "skins" + File.separator + "archive");
+
+             if (!dirnew.exists()) {
+            	 dirnew.mkdirs();
+            }
+
+             String filenamenew = args[0].toLowerCase() + "-" + System.currentTimeMillis() + ".dat";
+             File filenew = new File(dirnew, filenamenew);
+
+        	 file.renameTo(filenew);
         } catch (Exception var6) {
              LogWriter.except(var6);
              var6.printStackTrace();

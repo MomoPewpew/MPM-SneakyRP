e44ce4a062fa51a2dc857b6ef879d41c27f7e1f6
marijntepas <marijntepas@gmail.com>
2021-07-22 18:35:30 +0200

    Added functionality to blacklisted propstrings

--- src/main/java/noppes/mpm/Prop.java
+++ src/main/java/noppes/mpm/Prop.java
@@ -118,6 +118,10 @@ public class Prop {
      }
 
      public boolean parsePropString(String propString) {
+    	 for (String string : MorePlayerModels.blacklistedPropStrings) {
+    		 if (propString.contains(string)) return false;
+    	 }
+
     	 String nameSpacedId = "";
     	 short dataValue = 0;
 

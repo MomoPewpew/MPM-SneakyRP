151053f7e27753662a2872d293e85bcc48695630
Monica Moniot <mamoniot@protonmail.com>
2021-08-04 13:28:06 -0400

    fixed indent

--- src/main/java/noppes/mpm/PacketHandlerServer.java
+++ src/main/java/noppes/mpm/PacketHandlerServer.java
@@ -19,234 +19,234 @@ import noppes.mpm.constants.EnumAnimation;
 import noppes.mpm.constants.EnumPackets;
 
 public class PacketHandlerServer {
-     @SubscribeEvent
-     public void onPacketData(ServerCustomPacketEvent event) {
-          EntityPlayerMP player = ((NetHandlerPlayServer)event.getHandler()).playerEntity;
-          ByteBuf buf = event.getPacket().payload();
-          player.getServer().addScheduledTask(() -> {
-               EnumPackets type = null;
+	@SubscribeEvent
+	public void onPacketData(ServerCustomPacketEvent event) {
+		EntityPlayerMP player = ((NetHandlerPlayServer)event.getHandler()).playerEntity;
+		ByteBuf buf = event.getPacket().payload();
+		player.getServer().addScheduledTask(() -> {
+			EnumPackets type = null;
 
-               try {
-                    type = EnumPackets.values()[buf.readInt()];
-                    this.handlePacket(buf, player, type);
-               } catch (Exception var5) {
-                    LogWriter.error("Error with EnumPackets." + type, var5);
-               }
+			try {
+				type = EnumPackets.values()[buf.readInt()];
+				this.handlePacket(buf, player, type);
+			} catch (Exception var5) {
+				LogWriter.error("Error with EnumPackets." + type, var5);
+			}
 
-          });
-     }
+		});
+	}
 
-     private void handlePacket(ByteBuf buffer, EntityPlayerMP player, EnumPackets type) throws Exception {
-          if (type == EnumPackets.PING) {
-               int version = buffer.readInt();
-               if (version == MorePlayerModels.Version) {
-            	   ModelData data = ModelData.get(player);
-                    data.readFromNBT(Server.readNBT(buffer));
-                    if (!player.worldObj.getGameRules().getBoolean("mpmAllowEntityModels")) {
-                         data.entityClass = null;
-                    }
+	private void handlePacket(ByteBuf buffer, EntityPlayerMP player, EnumPackets type) throws Exception {
+		if (type == EnumPackets.PING) {
+			int version = buffer.readInt();
+			if (version == MorePlayerModels.Version) {
+				ModelData data = ModelData.get(player);
+				data.readFromNBT(Server.readNBT(buffer));
+				if (!player.worldObj.getGameRules().getBoolean("mpmAllowEntityModels")) {
+					data.entityClass = null;
+				}
 
-                    data.save();
-                    Server.sendAssociatedData(player, EnumPackets.SEND_PLAYER_DATA, player.getUniqueID(), data.writeToNBT());
-               }
+				data.save();
+				Server.sendAssociatedData(player, EnumPackets.SEND_PLAYER_DATA, player.getUniqueID(), data.writeToNBT());
+			}
 
-               ItemStack back = (ItemStack)player.inventory.mainInventory.get(0);
-               if (back != null) {
-                    Server.sendAssociatedData(player, EnumPackets.BACK_ITEM_UPDATE, player.getUniqueID(), back.writeToNBT(new NBTTagCompound()));
-               }
+			ItemStack back = (ItemStack)player.inventory.mainInventory.get(0);
+			if (back != null) {
+				Server.sendAssociatedData(player, EnumPackets.BACK_ITEM_UPDATE, player.getUniqueID(), back.writeToNBT(new NBTTagCompound()));
+			}
 
-               Server.sendData(player, EnumPackets.PING, MorePlayerModels.Version);
-          } else if (type == EnumPackets.UPDATE_PLAYER_DATA) {
-               ModelData data = ModelData.get(player);
-               data.readFromNBT(Server.readNBT(buffer));
-               if (!player.worldObj.getGameRules().getBoolean("mpmAllowEntityModels")) {
-                    data.entityClass = null;
-               }
+			Server.sendData(player, EnumPackets.PING, MorePlayerModels.Version);
+		} else if (type == EnumPackets.UPDATE_PLAYER_DATA) {
+			ModelData data = ModelData.get(player);
+			data.readFromNBT(Server.readNBT(buffer));
+			if (!player.worldObj.getGameRules().getBoolean("mpmAllowEntityModels")) {
+				data.entityClass = null;
+			}
 
-               data.save();
-               Server.sendAssociatedData(player, EnumPackets.SEND_PLAYER_DATA, player.getUniqueID(), data.writeToNBT());
-          } else if (type == EnumPackets.ANIMATION) {
-               EnumAnimation animation = EnumAnimation.values()[buffer.readInt()];
-               if (animation == EnumAnimation.SLEEPING_SOUTH) {
-                    float rotation;
-                    for(rotation = player.rotationYaw; rotation < 0.0F; rotation += 360.0F) {
-                    }
+			data.save();
+			Server.sendAssociatedData(player, EnumPackets.SEND_PLAYER_DATA, player.getUniqueID(), data.writeToNBT());
+		} else if (type == EnumPackets.ANIMATION) {
+			EnumAnimation animation = EnumAnimation.values()[buffer.readInt()];
+			if (animation == EnumAnimation.SLEEPING_SOUTH) {
+				float rotation;
+				for(rotation = player.rotationYaw; rotation < 0.0F; rotation += 360.0F) {
+				}
 
-                    while(rotation > 360.0F) {
-                         rotation -= 360.0F;
-                    }
+				while(rotation > 360.0F) {
+					rotation -= 360.0F;
+				}
 
-                    int rotate = (int)((rotation + 45.0F) / 90.0F);
-                    if (rotate == 1) {
-                         animation = EnumAnimation.SLEEPING_WEST;
-                    }
+				int rotate = (int)((rotation + 45.0F) / 90.0F);
+				if (rotate == 1) {
+					animation = EnumAnimation.SLEEPING_WEST;
+				}
 
-                    if (rotate == 2) {
-                         animation = EnumAnimation.SLEEPING_NORTH;
-                    }
+				if (rotate == 2) {
+					animation = EnumAnimation.SLEEPING_NORTH;
+				}
 
-                    if (rotate == 3) {
-                         animation = EnumAnimation.SLEEPING_EAST;
-                    }
-               }
+				if (rotate == 3) {
+					animation = EnumAnimation.SLEEPING_EAST;
+				}
+			}
 
-               ModelData data = ModelData.get(player);
-               if (data.animationEquals(animation)) {
-                    animation = EnumAnimation.NONE;
-               }
+			ModelData data = ModelData.get(player);
+			if (data.animationEquals(animation)) {
+				animation = EnumAnimation.NONE;
+			}
 
-               Server.sendAssociatedData(player, EnumPackets.ANIMATION, player.getUniqueID(), animation);
-               data.setAnimation(animation);
-         } else if (type == EnumPackets.PROP_CLEAR) {
-             ModelData data = ModelData.get(player);
+			Server.sendAssociatedData(player, EnumPackets.ANIMATION, player.getUniqueID(), animation);
+			data.setAnimation(animation);
+		} else if (type == EnumPackets.PROP_CLEAR) {
+			ModelData data = ModelData.get(player);
 
-             data.clearPropsServer();
-         } else if (type == EnumPackets.PROP_ADD) {
-             ModelData data = ModelData.get(player);
+			data.clearPropsServer();
+		} else if (type == EnumPackets.PROP_ADD) {
+			ModelData data = ModelData.get(player);
 
-             Prop prop = new Prop();
-             NBTTagCompound compound = Server.readNBT(buffer);
-             prop.readFromNBT(compound);
-             data.propBase.props.add(prop);
+			Prop prop = new Prop();
+			NBTTagCompound compound = Server.readNBT(buffer);
+			prop.readFromNBT(compound);
+			data.propBase.props.add(prop);
 
-      	     Server.sendAssociatedData(player, EnumPackets.PROP_ADD, player.getUniqueID(), compound);
-	     } else if (type == EnumPackets.PROP_SYNC) {
-             ModelData data = ModelData.get(player);
+			Server.sendAssociatedData(player, EnumPackets.PROP_ADD, player.getUniqueID(), compound);
+		} else if (type == EnumPackets.PROP_SYNC) {
+			ModelData data = ModelData.get(player);
 
-             NBTTagCompound compound = Server.readNBT(buffer);
-             data.propsFromNBT(compound);
+			NBTTagCompound compound = Server.readNBT(buffer);
+			data.propsFromNBT(compound);
 
-	 	     Server.sendAssociatedData(player, EnumPackets.PROP_SYNC, player.getUniqueID(), compound);
-	     } else if (type == EnumPackets.PROP_REMOVE) {
-             ModelData data = ModelData.get(player);
+			Server.sendAssociatedData(player, EnumPackets.PROP_SYNC, player.getUniqueID(), compound);
+		} else if (type == EnumPackets.PROP_REMOVE) {
+			ModelData data = ModelData.get(player);
 
-             Integer index = buffer.readInt();
-             data.propBase.removePropServer(index);
-	     } else if (type == EnumPackets.PROP_GIVE) {
-             Integer index = buffer.readInt();
+			Integer index = buffer.readInt();
+			data.propBase.removePropServer(index);
+		} else if (type == EnumPackets.PROP_GIVE) {
+			Integer index = buffer.readInt();
 
-             CommandProp.giveProp(null, index, player);
-	     } else if (type == EnumPackets.PROPGROUP_GIVE) {
-             Integer index = buffer.readInt();
+			CommandProp.giveProp(null, index, player);
+		} else if (type == EnumPackets.PROPGROUP_GIVE) {
+			Integer index = buffer.readInt();
 
-             CommandProp.givePropGroup(null, index, player);
-	     } else if (type == EnumPackets.PROPGROUP_SAVE) {
-             NBTTagCompound compound = Server.readNBT(buffer);
-             String uuid = compound.getString("uuid");
-             NBTTagCompound propCompound = compound.getCompoundTag("propGroup");
+			CommandProp.givePropGroup(null, index, player);
+		} else if (type == EnumPackets.PROPGROUP_SAVE) {
+			NBTTagCompound compound = Server.readNBT(buffer);
+			String uuid = compound.getString("uuid");
+			NBTTagCompound propCompound = compound.getCompoundTag("propGroup");
 
-             File dir = null;
-             dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "propGroups");
-             if (!dir.exists()) {
-                  dir.mkdirs();
-             }
+			File dir = null;
+			dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "propGroups");
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
 
-             String filename = uuid + ".dat";
+			String filename = uuid + ".dat";
 
-             try {
-                  File file = new File(dir, filename);
-                  CompressedStreamTools.writeCompressed(propCompound, new FileOutputStream(file));
-             } catch (Exception var6) {
-                  LogWriter.except(var6);
-                  var6.printStackTrace();
-             }
-	     } else if (type == EnumPackets.SKIN_FILENAME_UPDATE) {
-             MorePlayerModels.syncSkinFileNames(player);
-	     } else if (type == EnumPackets.PROPGROUPS_FILENAME_UPDATE) {
-             MorePlayerModels.syncPropGroupFileNames(player);
-	     } else if (type == EnumPackets.UPDATE_PLAYER_DATA_CLIENT) {
-            NBTTagCompound compound = Server.readNBT(buffer);
+			try {
+				File file = new File(dir, filename);
+				CompressedStreamTools.writeCompressed(propCompound, new FileOutputStream(file));
+			} catch (Exception var6) {
+				LogWriter.except(var6);
+				var6.printStackTrace();
+			}
+		} else if (type == EnumPackets.SKIN_FILENAME_UPDATE) {
+			MorePlayerModels.syncSkinFileNames(player);
+		} else if (type == EnumPackets.PROPGROUPS_FILENAME_UPDATE) {
+			MorePlayerModels.syncPropGroupFileNames(player);
+		} else if (type == EnumPackets.UPDATE_PLAYER_DATA_CLIENT) {
+			NBTTagCompound compound = Server.readNBT(buffer);
 
 			String filename = compound.getString("skinName") + ".dat";
 			File file;
 
 			File dir = null;
 
-	        try {
+			try {
 				dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "skins" + File.separator + "unrestricted");
 
-		        if (!dir.exists()) {
-		              dir.mkdirs();
-		         }
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
 
-	             file = new File(dir, filename);
+				file = new File(dir, filename);
 
-	             if (!file.exists()) {
-	            	 dir = null;
-	            	 dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "skins");
+				if (!file.exists()) {
+					dir = null;
+					dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "skins");
 
-	 		        if (!dir.exists()) {
-	 		              dir.mkdirs();
-	 		         }
+					if (!dir.exists()) {
+						dir.mkdirs();
+					}
 
-	 	             file = new File(dir, filename);
-	             }
+					file = new File(dir, filename);
+				}
 
-	             if (!file.exists()) {
-	            	 dir = null;
-	            	 dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "skins" + File.separator + "restricted");
+				if (!file.exists()) {
+					dir = null;
+					dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "skins" + File.separator + "restricted");
 
-	 		        if (!dir.exists()) {
-	 		              dir.mkdirs();
-	 		         }
+					if (!dir.exists()) {
+						dir.mkdirs();
+					}
 
-	 	             file = new File(dir, filename);
-	             }
+					file = new File(dir, filename);
+				}
 
-	             if (!file.exists()) {
-	            	 return;
-	             }
+				if (!file.exists()) {
+					return;
+				}
 
-	             NBTTagCompound skinCompound = new NBTTagCompound();
+				NBTTagCompound skinCompound = new NBTTagCompound();
 
-	             skinCompound = CompressedStreamTools.readCompressed(new FileInputStream(file));
+				skinCompound = CompressedStreamTools.readCompressed(new FileInputStream(file));
 
-	             Server.sendData(player, EnumPackets.UPDATE_PLAYER_DATA_CLIENT, skinCompound);
-	        } catch (Exception var4) {
-	             LogWriter.except(var4);
-	        }
-	     } else if (type == EnumPackets.PROPGROUP_LOAD_CLIENT) {
-	            NBTTagCompound compound = Server.readNBT(buffer);
+				Server.sendData(player, EnumPackets.UPDATE_PLAYER_DATA_CLIENT, skinCompound);
+			} catch (Exception var4) {
+				LogWriter.except(var4);
+			}
+		} else if (type == EnumPackets.PROPGROUP_LOAD_CLIENT) {
+			NBTTagCompound compound = Server.readNBT(buffer);
 
-				String filename = compound.getString("propName") + ".dat";
-				File file;
+			String filename = compound.getString("propName") + ".dat";
+			File file;
 
-				File dir = null;
+			File dir = null;
 
-		        try {
-					dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "propGroupsNamed");
+			try {
+				dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "propGroupsNamed");
 
-			        if (!dir.exists()) {
-			              dir.mkdirs();
-			         }
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
 
-		             file = new File(dir, filename);
+				file = new File(dir, filename);
 
-		             if (!file.exists()) {
-		            	 dir = null;
-		            	 dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "propGroupsNamed" + File.separator + "restricted");
+				if (!file.exists()) {
+					dir = null;
+					dir = new File(dir, ".." + File.separator + "moreplayermodels" + File.separator + "propGroupsNamed" + File.separator + "restricted");
 
-		 		        if (!dir.exists()) {
-		 		              dir.mkdirs();
-		 		         }
+					if (!dir.exists()) {
+						dir.mkdirs();
+					}
 
-		 	             file = new File(dir, filename);
-		             }
+					file = new File(dir, filename);
+				}
 
-		             if (!file.exists()) {
-		            	 return;
-		             }
+				if (!file.exists()) {
+					return;
+				}
 
-		             NBTTagCompound propCompound = new NBTTagCompound();
+				NBTTagCompound propCompound = new NBTTagCompound();
 
-		             propCompound = CompressedStreamTools.readCompressed(new FileInputStream(file));
+				propCompound = CompressedStreamTools.readCompressed(new FileInputStream(file));
 
-		             Server.sendData(player, EnumPackets.PROPGROUP_LOAD_CLIENT, propCompound);
-		        } catch (Exception var4) {
-		             LogWriter.except(var4);
-		        }
-	     }
+				Server.sendData(player, EnumPackets.PROPGROUP_LOAD_CLIENT, propCompound);
+			} catch (Exception var4) {
+				LogWriter.except(var4);
+			}
+		}
 
 
-     }
+	}
 }

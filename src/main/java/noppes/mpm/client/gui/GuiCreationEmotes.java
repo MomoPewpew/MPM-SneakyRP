3bc64e58ca82d309aaa10507f0c5d3c61f6e465e
Monica Moniot <mamoniot@protonmail.com>
2021-08-05 12:55:21 -0400

    fixed crash

--- src/main/java/noppes/mpm/client/gui/GuiCreationEmotes.java
+++ src/main/java/noppes/mpm/client/gui/GuiCreationEmotes.java
@@ -324,9 +324,11 @@ public class GuiCreationEmotes extends GuiCreationScreenInterface implements ISl
 			} else if(btn.id == 609) {//copy anim command
 				clipboardCommand = cur_command.clone();
 			} else if(btn.id == 610) {//paste anim command
-				cur_command_list.set(selected, clipboardCommand.clone());
-				onEmoteChange();
-				this.initGui();
+				if(clipboardCommand != null) {
+					cur_command_list.set(selected, clipboardCommand.clone());
+					onEmoteChange();
+					this.initGui();
+				}
 			} else if(btn.id == 611) {//duplicate anim command
 				cur_command_list.add(selected++, cur_command.clone());
 				onEmoteChange();

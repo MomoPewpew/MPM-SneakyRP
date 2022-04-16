119fd4d1fc763806cc76141f0dd8949e367cb5d2
marijntepas <marijntepas@gmail.com>
2022-04-16 17:37:25 +0200

    Removed config button

--- src/main/java/noppes/mpm/client/gui/GuiMPM.java
+++ src/main/java/noppes/mpm/client/gui/GuiMPM.java
@@ -80,8 +80,8 @@ public class GuiMPM extends GuiNPCInterface implements ICustomScrollListener, IS
 		this.addButton(new GuiNpcButton(1, this.guiLeft + 26, this.guiTop + 176, 20, 20, "-"));
 		this.getButton(1).enabled = false;
 		//this.getButton(1).enabled = this.scroll.getList().size() > 1;
-		this.addButton(new GuiNpcButton(2, this.guiLeft + 48, this.guiTop + 176, 60, 20, "selectServer.edit"));
-		this.addButton(new GuiNpcButton(3, this.guiLeft + 110, this.guiTop + 176, 68, 20, "gui.config"));
+		this.addButton(new GuiNpcButton(2, this.guiLeft + 48, this.guiTop + 176, 129, 20, "selectServer.edit"));
+		//this.addButton(new GuiNpcButton(3, this.guiLeft + 110, this.guiTop + 176, 68, 20, "gui.config"));
 		this.addLabel(new GuiNpcLabel(1336, "This menu is scheduled for removal, so the + and - buttons", this.guiLeft - 50, this.guiTop - 25, 16711680));
 		this.addLabel(new GuiNpcLabel(1337, "have been disabled. Please stick to using one save slot,", this.guiLeft - 50, this.guiTop - 15, 16711680));
 		this.addLabel(new GuiNpcLabel(1338, "and save your skins to the server instead using /skinsave.", this.guiLeft - 50, this.guiTop - 5, 16711680));

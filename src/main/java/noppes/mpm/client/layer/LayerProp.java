6c149261d9d419fa3a94e402e9648c9314b76326
marijntepas <marijntepas@gmail.com>
2022-04-19 11:08:35 +0200

    Particle prop optimizations

--- src/main/java/noppes/mpm/client/layer/LayerProp.java
+++ src/main/java/noppes/mpm/client/layer/LayerProp.java
@@ -218,94 +218,114 @@ public class LayerProp extends LayerInterface {
 
 		if (propHide == false) {
 			if (System.currentTimeMillis() - prop.lastPlayed >= 1000 / prop.frequency) {
-				for (int i = 0 ; i < prop.amount ; i++) {
-					Minecraft minecraft = Minecraft.getMinecraft();
+				Minecraft minecraft = Minecraft.getMinecraft();
 
+				float sneakModifierY = 0.0F;
+				float sneakModifierZ = 0.0F;
+
+				if (prop.propBodyPart == this.model.bipedHead && this.playerdata.player == minecraft.thePlayer && minecraft.gameSettings.thirdPersonView == 0 && !(minecraft.currentScreen instanceof GuiNPCInterface))
+				return;
+
+				if (this.player.isSneaking()) {
+					if (prop.propBodyPart == this.model.bipedLeftLeg || prop.propBodyPart == this.model.bipedRightLeg) {
+						sneakModifierY = -0.125F;
+						sneakModifierZ = -0.25F;
+					} else if (prop.propBodyPart == this.model.bipedHead) {
+						sneakModifierY = -0.375F;
+					} else if (prop.propBodyPart == this.model.bipedLeftArm || prop.propBodyPart == this.model.bipedRightArm) {
+						sneakModifierY = -0.375F;
+					} else if (prop.propBodyPart == this.model.bipedBody) {
+						sneakModifierY = -0.375F;
+					}
+				}
+
+				float propOffsetXCorrected;
+				float propOffsetYCorrected;
+				float propOffsetZCorrected;
+
+				if (prop.partIndex < 0) {
+					propOffsetXCorrected = -prop.propOffsetX - prop.propBodyPart.offsetX - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_X]);
+					propOffsetYCorrected = prop.propOffsetY;
+					propOffsetZCorrected = prop.propOffsetZ - prop.propBodyPart.offsetZ - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_Z]);
+				} else {
+					//Calculate prop offset
+					float anglePrev;
+					float hyp;
+					//Apply pitch
+					if (prop.propOffsetZ == 0) {
+						if (prop.propOffsetY <= 0) {
+							anglePrev = 0.0F;
+							hyp = prop.propOffsetY;
+						} else {
+							anglePrev = (float) Math.PI;
+							hyp = -prop.propOffsetY;
+						}
+					} else {
+						anglePrev = (float) Math.atan2(prop.propOffsetZ, prop.propOffsetY);
+						hyp = (float) (prop.propOffsetZ / Math.sin(anglePrev));
+					}
+
+					float Zpitch = (float) (Math.sin(anglePrev + prop.propBodyPart.rotateAngleX) * hyp);
+					float Ypitch = (float) (Math.cos(anglePrev + prop.propBodyPart.rotateAngleX) * hyp);
+
+					//Apply yaw
+					if (prop.propOffsetX == 0) {
+						if (Zpitch >= 0) {
+							anglePrev = 0.0F;
+							hyp = Zpitch;
+						} else {
+							anglePrev = (float) Math.PI;
+							hyp = -Zpitch;
+						}
+					} else {
+						anglePrev = (float) Math.atan2(-prop.propOffsetX, Zpitch);
+						hyp = (float) (-prop.propOffsetX / Math.sin(anglePrev));
+					}
+
+					float Xyaw = (float) (Math.sin(anglePrev + prop.propBodyPart.rotateAngleY) * hyp);
+					propOffsetZCorrected = (float) (Math.cos(anglePrev + prop.propBodyPart.rotateAngleY) * hyp) - prop.propBodyPart.offsetZ - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_Z]);
+
+					//Apply roll
+					if (Xyaw > -0.0001 && Xyaw < 0.0001) {
+						if (Ypitch <= 0) {
+							anglePrev = 0.0F;
+							hyp = Ypitch;
+						} else {
+							anglePrev = (float) Math.PI;
+							hyp = -Ypitch;
+						}
+					} else {
+						anglePrev = (float) Math.atan2(Xyaw, Ypitch);
+						hyp = (float) (Xyaw / Math.sin(anglePrev));
+					}
+
+					propOffsetXCorrected = (float) (Math.sin(anglePrev - prop.propBodyPart.rotateAngleZ) * hyp) - prop.propBodyPart.offsetX - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_X]);
+					propOffsetYCorrected = (float) (Math.cos(anglePrev - prop.propBodyPart.rotateAngleZ) * hyp);
+				}
+
+				//Adjust for model yaw
+				float propOffsetXCorrected2 = (float) (propOffsetXCorrected * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + 2 * propOffsetZCorrected * Math.sin(Math.toRadians(this.player.renderYawOffset)));
+				float propOffsetZCorrected2 = (float) (propOffsetZCorrected * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + propOffsetXCorrected * Math.sin(Math.toRadians(-this.player.renderYawOffset)));
+
+				EntityLivingBase entity = this.playerdata.getEntity(this.player);
+				float partModifierXCorrected = 0;
+				float partModifierZCorrected = 0;
+				if (entity != null) {
+
+					Float yRender = (float) Math.toRadians(entity.prevRenderYawOffset + ((entity.renderYawOffset - entity.prevRenderYawOffset) * Animation.getPartialTickTime()));
+
+					if (prop.propBodyPart != null) {
+						partModifierXCorrected = (float) -((((Math.cos(yRender) * prop.propBodyPart.rotationPointX) + (Math.sin(yRender) * prop.propBodyPart.rotationPointZ)) / 16)) * this.playerdata.entityScaleX * EntityScaleManagerClient.getScaleMult(entity);
+						partModifierZCorrected = (float) -((((Math.cos(-yRender) * prop.propBodyPart.rotationPointZ) + (Math.sin(-yRender) * prop.propBodyPart.rotationPointX)) / 16)) * this.playerdata.entityScaleX * EntityScaleManagerClient.getScaleMult(entity);
+					}
+				} else {
+					partModifierXCorrected = (float) (prop.partModifierX * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + propOffsetZCorrected * Math.sin(Math.toRadians(-this.player.renderYawOffset)));
+					partModifierZCorrected = (float) (prop.partModifierZ * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + prop.partModifierX * Math.sin(Math.toRadians(-this.player.renderYawOffset)));
+				}
+
+				for (int i = 0 ; i < prop.amount ; i++) {
 					Double propMotionPitch = Math.toRadians((prop.pitch + (2 * prop.scatter * Math.random()) - prop.scatter));
 					Double propMotionYaw = Math.toRadians((prop.yaw + (2 * prop.scatter * Math.random()) - prop.scatter));
-
-					float sneakModifierY = 0.0F;
-					float sneakModifierZ = 0.0F;
-
-					if (prop.propBodyPart == this.model.bipedHead && this.playerdata.player == minecraft.thePlayer && minecraft.gameSettings.thirdPersonView == 0 && !(minecraft.currentScreen instanceof GuiNPCInterface))
-					return;
-
-					if (this.player.isSneaking()) {
-						if (prop.propBodyPart == this.model.bipedLeftLeg || prop.propBodyPart == this.model.bipedRightLeg) {
-							sneakModifierY = -0.125F;
-							sneakModifierZ = -0.25F;
-						} else if (prop.propBodyPart == this.model.bipedHead) {
-							sneakModifierY = -0.375F;
-						} else if (prop.propBodyPart == this.model.bipedLeftArm || prop.propBodyPart == this.model.bipedRightArm) {
-							sneakModifierY = -0.375F;
-						} else if (prop.propBodyPart == this.model.bipedBody) {
-							sneakModifierY = -0.375F;
-						}
-					}
-
-					float propOffsetXCorrected;
-					float propOffsetYCorrected;
-					float propOffsetZCorrected;
-
-					if (prop.partIndex < 0) {
-						propOffsetXCorrected = -prop.propOffsetX - prop.propBodyPart.offsetX - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_X]);
-						propOffsetYCorrected = prop.propOffsetY;
-						propOffsetZCorrected = prop.propOffsetZ - prop.propBodyPart.offsetZ - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_Z]);
-					} else {
-						//Calculate prop offset
-						float anglePrev;
-						float hyp;
-						//Apply pitch
-						if (prop.propOffsetZ == 0) {
-							if (prop.propOffsetY <= 0) {
-								anglePrev = 0.0F;
-								hyp = prop.propOffsetY;
-							} else {
-								anglePrev = (float) Math.PI;
-								hyp = -prop.propOffsetY;
-							}
-						} else {
-							anglePrev = (float) Math.atan2(prop.propOffsetZ, prop.propOffsetY);
-							hyp = (float) (prop.propOffsetZ / Math.sin(anglePrev));
-						}
-
-						float Zpitch = (float) (Math.sin(anglePrev + prop.propBodyPart.rotateAngleX) * hyp);
-						float Ypitch = (float) (Math.cos(anglePrev + prop.propBodyPart.rotateAngleX) * hyp);
-
-						//Apply yaw
-						if (prop.propOffsetX == 0) {
-							if (Zpitch >= 0) {
-								anglePrev = 0.0F;
-								hyp = Zpitch;
-							} else {
-								anglePrev = (float) Math.PI;
-								hyp = -Zpitch;
-							}
-						} else {
-							anglePrev = (float) Math.atan2(-prop.propOffsetX, Zpitch);
-							hyp = (float) (-prop.propOffsetX / Math.sin(anglePrev));
-						}
-
-						float Xyaw = (float) (Math.sin(anglePrev + prop.propBodyPart.rotateAngleY) * hyp);
-						propOffsetZCorrected = (float) (Math.cos(anglePrev + prop.propBodyPart.rotateAngleY) * hyp) - prop.propBodyPart.offsetZ - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_Z]);
-
-						//Apply roll
-						if (Xyaw > -0.0001 && Xyaw < 0.0001) {
-							if (Ypitch <= 0) {
-								anglePrev = 0.0F;
-								hyp = Ypitch;
-							} else {
-								anglePrev = (float) Math.PI;
-								hyp = -Ypitch;
-							}
-						} else {
-							anglePrev = (float) Math.atan2(Xyaw, Ypitch);
-							hyp = (float) (Xyaw / Math.sin(anglePrev));
-						}
-
-						propOffsetXCorrected = (float) (Math.sin(anglePrev - prop.propBodyPart.rotateAngleZ) * hyp) - prop.propBodyPart.offsetX - (this.playerdata.animStates == null ? 0.0F : this.playerdata.animStates[Emote.AXIS_COUNT*Emote.MODEL + Emote.OFF_X]);
-						propOffsetYCorrected = (float) (Math.cos(anglePrev - prop.propBodyPart.rotateAngleZ) * hyp);
-					}
 
 					Double propMotionXCorrected = null;
 					Double propMotionYCorrected = null;
@@ -346,26 +366,6 @@ public class LayerProp extends LayerInterface {
 
 						propMotionXCorrected = (Math.sin(anglePrev + prop.propBodyPart.rotateAngleZ) * hyp);
 						propMotionYCorrected = (Math.cos(anglePrev + prop.propBodyPart.rotateAngleZ) * hyp);
-					}
-
-					//Adjust for model yaw
-					float propOffsetXCorrected2 = (float) (propOffsetXCorrected * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + 2 * propOffsetZCorrected * Math.sin(Math.toRadians(this.player.renderYawOffset)));
-					float propOffsetZCorrected2 = (float) (propOffsetZCorrected * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + propOffsetXCorrected * Math.sin(Math.toRadians(-this.player.renderYawOffset)));
-
-					EntityLivingBase entity = this.playerdata.getEntity(this.player);
-					float partModifierXCorrected = 0;
-					float partModifierZCorrected = 0;
-					if (entity != null) {
-
-						Float yRender = (float) Math.toRadians(entity.prevRenderYawOffset + ((entity.renderYawOffset - entity.prevRenderYawOffset) * Animation.getPartialTickTime()));
-
-						if (prop.propBodyPart != null) {
-							partModifierXCorrected = (float) -((((Math.cos(yRender) * prop.propBodyPart.rotationPointX) + (Math.sin(yRender) * prop.propBodyPart.rotationPointZ)) / 16)) * this.playerdata.entityScaleX * EntityScaleManagerClient.getScaleMult(entity);
-							partModifierZCorrected = (float) -((((Math.cos(-yRender) * prop.propBodyPart.rotationPointZ) + (Math.sin(-yRender) * prop.propBodyPart.rotationPointX)) / 16)) * this.playerdata.entityScaleX * EntityScaleManagerClient.getScaleMult(entity);
-						}
-					} else {
-						partModifierXCorrected = (float) (prop.partModifierX * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + propOffsetZCorrected * Math.sin(Math.toRadians(-this.player.renderYawOffset)));
-						partModifierZCorrected = (float) (prop.partModifierZ * Math.cos(Math.toRadians(-this.player.renderYawOffset)) + prop.partModifierX * Math.sin(Math.toRadians(-this.player.renderYawOffset)));
 					}
 
 					this.player.worldObj.spawnParticle(prop.particleType,
